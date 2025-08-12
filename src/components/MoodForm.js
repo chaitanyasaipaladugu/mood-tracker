f352760eab84858a95436daede33148a20aaccbe
chaitanyasaipaladugu <pcschaitanyasai@gmail.com>
2025-08-12 10:46:18 +0530

    wrote moodform and history codes

--- src/components/MoodForm.js
+++ src/components/MoodForm.js
@@ -6,4 +6,19 @@ import { addMoodEntry } from "../features/moodSlice";
 export default function MoodForm() {
   const [mood, setMood] = useState("");
   const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const today = new Date().toISOString().split("T")[0];
+    dispatch(addMoodEntry({ id: today, mood }));
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <select value={mood} onChange={(e) => setMood(e.target.value)}>
+        <option value="Happy">😊 Happy</option>
+        <option value="Neutral">😐 Neutral</option>
+        <option value="Sad">😢 Sad</option>
+      </select>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
--- src/components/MoodHistory.js
+++ src/components/MoodHistory.js
@@ -1,1 +1,31 @@
 // List of entries + filters
+import React from "react";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+
+export default function MoodHistory() {
+  const moods = useSelector((state) => state.moods);
+  const [filter, setFilter] = useState("");
+  const filtered = filter
+    ? moods.filter((mood) => mood.mood === filter)
+    : moods;
+  return (
+    <div>
+      <h3>Mood history</h3>
+      <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+        <option value="all">All</option>
+        <option value="Happy">😊 Happy</option>
+        <option value="Neutral">😐 Neutral</option>
+        <option value="Sad">😢 Sad</option>
+      </select>
+      <ul>
+        {filtered.map((entry, index) => {
+          <li key={index}>
+            {" "}
+            {entry.date} - {entry.mood}
+          </li>;
+        })}
+      </ul>
+    </div>
+  );
+}
